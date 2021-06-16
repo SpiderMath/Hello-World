b9aa0d0ebf39475a5b27eca2ae104a06ff80339e
Prannaya Gupta <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-16 16:20:35 +0800

    Create Hello-World.pas

---
+++ Source/Hello-World.pas
@@ -0,0 +1,7 @@
+//name of the program
+program hello_world; //<-- dont forget a semicolon
+
+//main program area
+begin
+  write("Hello, World!");
+end. // End of a main program area should required a "." symbol
