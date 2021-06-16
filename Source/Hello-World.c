ce1e3b235a09d973b25ab21271706723247185b2
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-16 13:14:57 +0530

    Added C to the list

---
+++ Source/Hello-World.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// Importing the stdio library, it gives us access to various methods to print stuff etc
+
+// main() is the entry point of the program, and it will be where the main logic has to work
+int main() {
+	// printf() prints the text to the console, without moving the curson to the next line
+	printf("Hello, World!");
+	return 0;
+	// We are returning 0, since the program is declared as `int main` which means it returns an int value. By return 0, we do just that
+}
+
+// C, as with C++ is one of the fastest programming languages, and is also one of the Oldest. It is a precursor to C++, and is still used for a variety of reasons
