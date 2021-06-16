63cf57de88fce164e5a3d32b653389c63085cefb
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-16 17:39:17 +0530

    Added Hello World in Julia

--- Source/Hello-World.jl
+++ Source/Hello-World.jl
@@ -1,1 +1,5 @@
+# Julia is a high-level, high-performance, dynamic programming language. While it is a general-purpose language and can be used to write any application, many of its features are well suited for numerical analysis and computational science
+
+# Println basically prints out `Hello, World!` to your console/terminal and puts the cursor to the NEXT line. So if you print something else, if will be
+# Done on the next line
 println("Hello, World!")
