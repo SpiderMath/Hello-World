426c08016d60f9b105cca095c28e334f6e1421a3
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-16 15:21:53 +0530

    Added Hello World in Golang

---
+++ Source/Hello-World.go
@@ -0,0 +1,11 @@
+// This line informs the compiler that the file is the main project file and the entry point for the program
+package main;
+
+// Imports the library named 'fmt', which is basically the library for Input Output functions
+import "fmt";
+
+// This is the main function of the file, the Entry point.
+func main() {
+	// fmt.Printf is basically the equivalent of printf in C, it DOES NOT move the cursor to the next line, after printing the text which is provided
+	fmt.Printf("Hello, World!");
+}
