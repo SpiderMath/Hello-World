394edc1d6d81e30707355acd424604956cbf21e0
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-16 18:08:23 +0530

    Added Hello World in Kotlin

--- Source/Hello-World.kt
+++ Source/Hello-World.kt
@@ -1,5 +1,8 @@
-package main
+// Kotlin is a cross-platform, statically typed, general-purpose programming language with type inference. Kotlin is designed to interoperate fully with Java, and the JVM version of Kotlin's standard library depends on the Java Class Library, but type inference allows its syntax to be more concise
 
+// main is the entry point of the program. All the logic of the file is present here!
 fun main() {
-  println("Hello, World!")
+  // println prints text to the console/terminal, with a line break
+  // So if you print something AFTER that, it will go to the next line, on its own
+  println("Hello, World!");
 }
