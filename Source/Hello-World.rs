0baac466f34b6f1bbfbd8ae1649a038176046392
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-16 13:26:42 +0530

    Added Hello-World in Rust, with proper comments

---
+++ Source/Hello-World.rs
@@ -0,0 +1,9 @@
+// main is the Entry point of the program, so the program basically starts from here
+fn main() {
+	// println is a Macro so it uses ! after it. It basically prints out whatever string is passed in as the first argument
+	println!("Hello, World!");
+	// println moves the cursor to the next line, so the next print or println would cause the program to output in the next line
+}
+
+// Rust is a language focused on speed, security and concurrency of the program. It provides the speed of a low level programming language like C, C++ while providing High level abstractions like of those in Python and Ruby
+// It thus can be a viable alternative to other languages in many ways
