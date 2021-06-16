e7d3a067a48e70462089de289847827754bb8b13
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-16 15:52:53 +0530

    Fixed stuff in Fortran, with adequate commenting

--- Source/Hello-World.f90
+++ Source/Hello-World.f90
@@ -1,4 +1,9 @@
+! Fortran general-purpose, compiled imperative programming language that is especially suited to numeric computation and scientific computing.
+! It is a REALLY old language, made in 1954 [It is older than a lot of the Grandparents we have]. This file has the extension f90 because the final code standard on this was made in 1990
+! This is where you declare the program kinda
 program hello
-  ! Too lazy to add a comment 😁 . Checkout https://fortran-lang.org/learn/quickstart/hello_world if u wanna konow more about it 😎.
-  print *, 'Hello, World!'
+	! print is to print stuff out, to the console
+	print *, 'Hello, World!'
+
+! end basically tells the code to END the program
 end program hello
