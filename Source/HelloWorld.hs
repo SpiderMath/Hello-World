81cf4264cba7934568400d8f4110775f34a32469
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-16 19:43:54 +0530

    Added Hello World in Haskell

--- Source/HelloWorld.hs
+++ Source/HelloWorld.hs
@@ -1,2 +1,7 @@
+--  is a general-purpose, statically typed, purely functional programming language with type inference and lazy evaluation
+
+-- This line specifies that main is of the type IO, which means Input Output actions
+-- This is a recommended practice even though the program could actually work without this
 main :: IO ()
+-- This line does the actual work, putStrLn puts the given string in the line with the line break
 main = putStrLn "Hello, World!"
