11621aa893085676b57ff09c0a7453c7c40380c7
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-16 20:04:53 +0530

    Added Hello World in Lua

--- Source/Hello-World.lua
+++ Source/Hello-World.lua
@@ -1,1 +1,4 @@
+-- Lua is a lightweight, high-level, multi-paradigm programming language designed primarily for embedded use in applications
+
+-- Print is a function which prints the input to the console/terminal
 print('Hello, World!')
