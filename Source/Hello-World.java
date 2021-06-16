15994607a6f7cc96d2e744a05bcf3792b0948aad
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-16 14:18:27 +0530

    Added Hello-World in Java

---
+++ Source/Hello-World.java
@@ -0,0 +1,9 @@
+// Java is an Object Oriented Programming language by James Gosling in 1995 as a General Purpose, High Level Language
+class HelloWorld {
+	// main function is the entry point of the program, so the code in here will be executed
+	public static void main(String[] args) {
+		// args is an ARRAY of STRINGs, and using this is Mandatory
+		System.out.println("Hello, World!");
+		// System.out provides us utilities, to output stuff to the Console/Terminal
+	}
+};
